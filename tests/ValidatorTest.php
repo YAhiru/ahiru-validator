2e170df3aedbe30bb7d83e9b0661214543eae73b
y_ahiru <yahiru1121@gmail.com>
2020-05-10 23:51:24 +0900

    add validate test

---
+++ tests/ValidatorTest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Yahiru\Validator;
+
+final class ValidatorTest extends TestCase
+{
+    public function testValidate()
+    {
+        $validator = new Validator();
+        $rule = new class implements RuleInterface {
+            public function isValid($value): bool
+            {
+                return false;
+            }
+
+            public function getMessage(string $attributeName): string
+            {
+                return $attributeName . 'を入力してください。';
+            }
+        };
+
+        $validator
+            ->define('name', '名前')
+            ->addRule($rule)
+        ;
+
+        $result = $validator->validate([]);
+
+        $this->assertTrue($result->hasErros());
+        $this->assertSame('名前を入力してください。', $result->getErrors()[0]);
+    }
+}
