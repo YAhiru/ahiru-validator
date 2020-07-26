20cc6098b7cdbb0e2e0c669bfbc720d786efd9fd
y_ahiru <yahiru1121@gmail.com>
2020-07-26 23:31:15 +0900

    update OverwriteMessage

--- src/Rule/OverwriteMessage.php
+++ src/Rule/OverwriteMessage.php
@@ -2,10 +2,12 @@
 declare(strict_types=1);
 namespace Yahiru\Validator\Rule;
 
+use Yahiru\Validator\DependsOtherValueInterface;
+use Yahiru\Validator\Input;
 use Yahiru\Validator\RuleInterface;
 use Yahiru\Validator\Value;
 
-final class OverwriteMessage implements RuleInterface
+final class OverwriteMessage implements RuleInterface, DependsOtherValueInterface
 {
     private RuleInterface $rule;
     private string $message;
@@ -14,6 +16,13 @@ final class OverwriteMessage implements RuleInterface
     {
         $this->rule = $rule;
         $this->message = $message;
+    }
+
+    public function getDependsValue(Input $input)
+    {
+        return $this->rule instanceof DependsOtherValueInterface
+            ? $this->rule->getDependsValue($input)
+            : null;
     }
 
     public function isValid(Value $value) : bool
