24191a972d91d1758dcda1bb0f2e97b9186bad5d
y_ahiru <yahiru1121@gmail.com>
2020-07-27 23:02:00 +0900

    refactoring

--- src/MessageBuilder.php
+++ src/MessageBuilder.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 namespace Yahiru\Validator;
 
+use LogicException;
 use Yahiru\Validator\Rule\OverwriteMessage;
 
 final class MessageBuilder
@@ -16,13 +17,13 @@ final class MessageBuilder
     public function build(string $attribute, RuleInterface $rule) : string
     {
         if ($rule instanceof OverwriteMessage) {
-            $template = $rule->getMessage();
-        } else {
-            $template = $this->repository->get(get_class($rule));
+            return $rule->getMessage();
         }
 
+        $template = $this->repository->get(get_class($rule));
+
         if ($template === null) {
-            throw new \LogicException();
+            throw new LogicException();
         }
 
         /** @var array<string, string> $replacements */
